990470ef1913d8ab251f10427409fc449f3b29d3
mgrbo <mgrbo@web.de>
2021-11-13 14:35:19 +0100

    Add project files.

---
+++ BTHospitalFix/Main.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:AssemblyVersion("1.0.0.0")]
+
+namespace BTHospitalFix
+{
+    class Main
+    {
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("com.github.mcb5637.BTHospitalFix");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
